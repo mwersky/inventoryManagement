28c5014cccbfd0df221b4ec823f15931faf5d4a5
Mitchell Werksy <dungeonmasterwersky@gmail.com>
2021-01-10 10:47:34 -0800

    Initial Packages made.

---
+++ src/main/java/com/promineotech/inventoryManagement/App.java
@@ -0,0 +1,13 @@
+package com.promineotech.inventoryManagement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
