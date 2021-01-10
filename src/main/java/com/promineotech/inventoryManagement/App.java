704a258ce47cbc102f20dbdc16f9137f016bfc8a
Mitchell Werksy <dungeonmasterwersky@gmail.com>
2021-01-10 13:20:20 -0800

    Service layer completed

--- src/main/java/com/promineotech/inventoryManagement/App.java
+++ src/main/java/com/promineotech/inventoryManagement/App.java
@@ -1,13 +1,15 @@
 package com.promineotech.inventoryManagement;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.promineotech.inventoryManagement")
+@SpringBootApplication
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
